bd203ad61d14d703bced7bf992c1a57fa3684aca
Garoverr <jacques.sapin@epitech.eu>
2024-12-03 11:45:45 +0100

    ADD: little window management loop window close

--- client/main.cpp
+++ client/main.cpp
@@ -1,7 +1,25 @@
-//
-// Created by Yanis Moyon on 26/11/2024.
-//
+/*
+** EPITECH PROJECT, 2024
+** RTYPE
+** File description:
+** CLIENT main
+*/
+
+#include "raylib.h"
+#include "./window/Window.hpp"
+#include <iostream>
+
+void initLittleWindow(void)
+{
+	int width = 800;
+	int height = 450;
+	const char *title = "R-TYPE";
+	
+	Window window(width, height, title);
+	
+	window.loop();
+}
 
 int main(void) {
-    return 0;
+	return 0;
 }
