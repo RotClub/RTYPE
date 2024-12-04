fb515b04934e3ecd0dad68e76e0925d45f3d93e9
Kapsulon <theo.bary@epitech.eu>
2024-12-04 15:18:39 +0100

    ADD: implemented JSON parser and game data loading

---
+++ common/GameInfo/GameInfo.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Théo Bary on 04/12/2024.
+//
+
+#include "GameInfo.hpp"
+
+GameInfo::GameInfo(
+    const std::string &name,
+    const std::string &description,
+    const unsigned int max_players,
+    const std::vector<const std::string> &authors,
+    const float version
+) : _name(name), _description(description), _max_players(max_players), _authors(authors), _version(version)
+{
+}
+
+GameInfo::GameInfo(nlohmann::json& gameData)
+    :   _name(gameData["name"]),
+        _description(gameData["description"]),
+        _max_players(gameData["max_players"]),
+        _authors({}),
+        _version(gameData["version"])
+{
+    for (const auto &author : gameData["authors"]) {
+        _authors.push_back(author);
+    }
+}
