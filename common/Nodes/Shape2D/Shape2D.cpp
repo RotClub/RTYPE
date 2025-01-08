ba78f367b6475d1eebe80778281cbdaf2b60d385
Garoverr <jacques.sapin@epitech.eu>
2025-01-08 20:06:15 +0100

    ADD: intersects func

---
+++ common/Nodes/Shape2D/Shape2D.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2025
+** RTYPE
+** File description:
+** Shape2D
+*/
+
+#include "Shape2D.hpp"
+
+bool Shape2D::intersects(const Shape2D &other) const
+{
+    if (shape == ShapeType::RECTANGLE) {
+        return _intersectsRect(other);
+    } else if (shape == ShapeType::CIRCLE) {
+        return _intersectsCircle(other);
+    }
+    return false;
+}
