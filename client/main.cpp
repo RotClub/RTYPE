d36c8349643199a99c640fe9fcf2a80fc51c1411
Garoverr <jacques.sapin@epitech.eu>
2025-01-12 17:10:41 +0100

    ADD: implementation of ClientArgumentManager

--- client/main.cpp
+++ client/main.cpp
@@ -6,10 +6,16 @@
 */
 
 #include "Client.hpp"
+#include "ArgumentManager/ClientArgumentManager.hpp"
+
 #include <cstdlib>
 
-int main(void)
+
+int main(int argc, char **argv)
 {
+    ClientArgumentManager argManager(argc, argv);
+    if (!argManager.checkClientArguments())
+        return 84;
     Engine &engine = Engine::StartInstance(Types::VMState::CLIENT, "rtype");
     Client &client = Client::InitiateInstance("127.0.0.1", 25777);
     client.getClientConnectionTcp().connectToServer();
