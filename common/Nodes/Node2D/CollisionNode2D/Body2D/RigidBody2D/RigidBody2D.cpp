ee724fc5174ae3f4a791ec32f9c26ca18a38cd00
Yanis Moyon <yanis.moyon@epitech.eu>
2025-01-14 10:20:53 +0100

    FIX: Body2D constructor

--- common/Nodes/Node2D/CollisionNode2D/Body2D/RigidBody2D/RigidBody2D.cpp
+++ common/Nodes/Node2D/CollisionNode2D/Body2D/RigidBody2D/RigidBody2D.cpp
@@ -5,7 +5,7 @@
 #include "RigidBody2D.hpp"
 
 RigidBody2D::RigidBody2D(const std::string& name, const Types::Vector2& position, const Types::Vector2& velocity)
-	: Body2D(name) {
+: Body2D(name) {
 	this->metatable = "RigidBody2DMetaTable";
 	this->velocity = velocity;
 	this->position = position;
--- common/Nodes/Node2D/CollisionNode2D/Body2D/RigidBody2D/RigidBody2D.hpp
+++ common/Nodes/Node2D/CollisionNode2D/Body2D/RigidBody2D/RigidBody2D.hpp
@@ -10,11 +10,6 @@
 class RigidBody2D : public Body2D {
 public:
 	RigidBody2D(const std::string& name, const Types::Vector2& position, const Types::Vector2& velocity);
-
-	~RigidBody2D() = default;
-
 };
-
-
 
 #endif //RIGIDBODY2D_HPP
