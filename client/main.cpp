e381b2a984e9a0c2d3c4e3bb9525f03bb3d90ef3
lmaoleo <leo.maurel@epitech.eu>
2025-01-19 19:58:57 +0100

    ADD: colorblindness mode swithing in client arguements

--- client/main.cpp
+++ client/main.cpp
@@ -7,8 +7,11 @@
 
 #include "ArgumentManager/ArgumentManager.hpp"
 #include "Client.hpp"
+#include "Engine.hpp"
+#include "config/Config.hpp"
 
 #include <cstdlib>
+#include <string>
 
 
 int main(int argc, char **argv)
@@ -16,12 +19,21 @@ int main(int argc, char **argv)
     ArgumentManager manager(argc, argv);
     manager.addRequiredArgument("ip");
     manager.addRequiredArgument("port");
+    manager.addDefaultArgument("colorblindness", "none");
     manager.parseArguments();
 
     if (!manager.checkClientArguments()) {
         ArgumentManager::DisplayClientUsage();
         throw std::runtime_error("Invalid arguments");
     }
+
+    std::string colorBlindness = manager.getArgument("colorblindness");
+    if (colorBlindness == "protanopia")
+        Config::GetInstance().setColorBlindnessMode(Config::ColorBlindnessMode::PROTANOPIA);
+    else if (colorBlindness == "deuteranopia")
+        Config::GetInstance().setColorBlindnessMode(Config::ColorBlindnessMode::DEUTERANOPIA);
+    else if (colorBlindness == "tritanopia")
+        Config::GetInstance().setColorBlindnessMode(Config::ColorBlindnessMode::TRITANOPIA);
 
     int port = static_cast<int>(std::strtol(manager.getArgument("port").c_str(), nullptr, 10));
     Engine::StartInstance(Types::VMState::CLIENT, "");
