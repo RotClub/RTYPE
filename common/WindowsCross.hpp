df58ceaf7b46967f90b6bac261006a8729e4755b
Kl0ups <yanis.moyon@epitech.eu>
2025-01-19 12:16:52 +0100

    FIX: better winmm support

--- common/WindowsCross.hpp
+++ common/WindowsCross.hpp
@@ -42,7 +42,17 @@
 #define NOSCROLL          // SB_* and scrolling routines
 #define NOSERVICE         // All Service Controller routines, SERVICE_ equates, etc.
 #define NOSOUND           // Sound driver routines
-#define MMNOSOUND         // Sound driver from Windows audio API
+#define MMNODRV           // Installable driver support
+#define MMNOSOUND         // Sound support
+#define MMNOWAVE          // Waveform support
+#define MMNOMIDI          // MIDI support
+#define MMNOAUX           // Auxiliary audio support
+#define MMNOMIXER         // Mixer support
+#define MMNOTIMER         // Timer support
+#define MMNOJOY           // Joystick support
+#define MMNOMCI           // MCI support
+#define MMNOMMIO          // Multimedia file I/O support
+#define MMNOMMSYSTEM      // General MMSYSTEM functions
 #define NOTEXTMETRIC      // typedef TEXTMETRIC and associated routines
 #define NOWH              // SetWindowsHook and WH_*
 #define NOWINOFFSETS      // GWL_*, GCL_*, associated routines
